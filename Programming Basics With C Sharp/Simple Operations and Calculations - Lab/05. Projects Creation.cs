b992e1439c2c358882923b6a97d80093059debbf
AleksandarHristovGithub <aleksandar.hristov9@abv.bg>
2020-04-28 16:03:05 +0300

    More Solutions

---
+++ Programming Basics With C Sharp/Simple Operations and Calculations - Lab/05. Projects Creation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfArhitect = Console.ReadLine();
+            int numberOfProjects = int.Parse(Console.ReadLine());
+
+            int timeForProject = numberOfProjects * 3;
+            Console.WriteLine($"The architect {nameOfArhitect} will need {timeForProject} hours to complete {numberOfProjects} project/s.");
+        }
+    }
+}
