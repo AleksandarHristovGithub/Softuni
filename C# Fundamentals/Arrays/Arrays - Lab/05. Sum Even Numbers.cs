b2a8a07f0b538b519d9a2136b18b7063f6ed3e1e
AleksandarHristovGithub <aleksandar.hristov9@abv.bg>
2020-11-10 19:50:12 +0200

    Arrays Done 33%

---
+++ C# Fundamentals/Arrays/Arrays - Lab/05. Sum Even Numbers.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SumEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                if (currentNumber % 2 == 0)
+                {
+                    sum += currentNumber;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays/Arrays - Lab/06. Even and Odd Subtraction.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Text;
+
+namespace EvenAndOddSubstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                if (currentNumber % 2 == 0)
+                {
+                    evenSum += currentNumber;
+                }
+                else
+                {
+                    oddSum += currentNumber;
+                }
+            }
+            int differene = evenSum - oddSum;
+            Console.WriteLine(differene);
+        }
+    }
+}
